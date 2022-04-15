0ee7aa9ba1e53c797bd2557ca6763d2608a430f6
adinekinar <adinekinari@gmail.com>
2022-04-15 14:00:44 +0700

    homepage update

--- lib/homepage/homePage.dart
+++ lib/homepage/homePage.dart
@@ -13,12 +13,119 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0XFF3D405B),
+        child: const Icon(
+          Icons.question_mark_outlined,
+          color: Color(0XFFF4F1DE),
+        ),
+        onPressed: () {},
+      ),
       body: Stack(
         children: [
-          floatAppBar(
+          const floatAppBar(
             title: "Monoread",
             icon_1: Icon(Icons.menu_rounded, color: Colors.white, size: 25,),
+            routePg1: homePage(),
             icon_2: Icon(Icons.more_vert_rounded, color: Colors.white, size: 25,),
+            routePg2: homePage(),
+          ),
+          Positioned(
+            left: 40, right: 40,
+            top: 139,
+            child: Column(
+              children: [
+                Container(width: 280, height: 157, color: Colors.yellowAccent,),
+                const SizedBox(height: 42,),
+                MaterialButton(
+                  child: Container(
+                    height: 87,
+                    width: 280,
+                    decoration: BoxDecoration(
+                      color: const Color(0XFF3D405B),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                                child: Text("The Model Reader", style: TextStyle(letterSpacing: 2, color: Color(0XFFF4F1DE)),),
+                              margin: EdgeInsets.symmetric(horizontal: 15),
+                            ),
+                            MaterialButton(
+                              child: Container(
+                                height: 35,
+                                width: 167,
+                                decoration: BoxDecoration(
+                                  color: Color(0XFFF4F1DE),
+                                  borderRadius: BorderRadius.circular(10)
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Icon(Icons.upload),
+                                    Text("Import Your Model")
+                                  ],
+                                ),
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 20,),
+                        Icon(Icons.wb_sunny_outlined, size: 40, color: Colors.white,),
+                      ],
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+                SizedBox(height: 48,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        MaterialButton(
+                          child: Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color(0XFFF4F1DE),
+                            ),
+                            child: Icon(CupertinoIcons.arrow_up_doc_fill, size: 60, color: Color(0XFF3D405B),),
+                          ),
+                          onPressed: () {},
+                        ),
+                        SizedBox(height: 10,),
+                        Text("Added Model")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        MaterialButton(
+                          child: Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color(0XFF81B29A),
+                            ),
+                            child: Icon(CupertinoIcons.globe, size: 60, color: Color(0XFFF4F1DE),),
+                          ),
+                          onPressed: () {},
+                        ),
+                        SizedBox(height: 10,),
+                        Text("Network Model")
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )
           )
         ],
       ),
--- lib/template/floatAppBar.dart
+++ lib/template/floatAppBar.dart
@@ -2,19 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class floatAppBar extends StatelessWidget {
-  const floatAppBar({Key? key, required this.title, required this.icon_1, required this.icon_2}) : super(key: key);
+  const floatAppBar({Key? key, required this.title, required this.icon_1, required this.icon_2, required this.routePg1, required this.routePg2}) : super(key: key);
   final String title;
   final Widget icon_1;
   final Widget icon_2;
+  final Widget routePg1;
+  final Widget routePg2;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.symmetric(vertical: 34, horizontal: 24),
+        margin: const EdgeInsets.symmetric(vertical: 34, horizontal: 24),
         height: 86,
         decoration: BoxDecoration(
-            color: Color(0XFF3D405B),
+            color: const Color(0XFF3D405B),
             borderRadius: BorderRadius.circular(40)
         ),
         child: Row(
@@ -22,18 +24,28 @@ class floatAppBar extends StatelessWidget {
           children: [
             IconButton(
               icon: icon_1,
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => routePg1),
+                );
+              },
             ),
             Text(
               title,
-              style: TextStyle(
+              style: const TextStyle(
                   color: Colors.white,
                   letterSpacing: 3
               ),
             ),
             IconButton(
               icon: icon_2,
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => routePg2),
+                );
+              },
             )
           ],
         ),
